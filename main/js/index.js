e58c741cf7e2186189ad984d550e031a0fb505bf
Vadym <vadim.rolex.2005@gmail.com>
2026-02-07 17:08:47 +0200

    fix: changed wish position for phone. Ready for refactoring.

--- main/js/index.js
+++ main/js/index.js
@@ -520,7 +520,7 @@ function createDraggableItems(container) {
 
     const positions = [];
 
-    const maxUniqueItems = 16;
+    const maxUniqueItems = 1;
     const limitedItems = items.slice(0, maxUniqueItems);
 
     const allItems = [];
